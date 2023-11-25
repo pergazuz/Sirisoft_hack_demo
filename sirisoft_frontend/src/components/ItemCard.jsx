f034b76e6aa92e90106d3b0765c4ca1fa9257424
Panupong Kaewkhao <tongfah@Panupongs-MacBook-Air.local>
2023-11-25 22:37:40 +0700

    Update Layout home

--- sirisoft_frontend/src/components/ItemCard.jsx
+++ sirisoft_frontend/src/components/ItemCard.jsx
@@ -33,14 +33,17 @@ function ItemCard({ title, description, imageUrl, topBarText, HeartBeat, BedExit
                     <div className="bg-gradient-custom p-5 rounded-md m-1 flex-1 drop-shadow-xl text-white text-sm">Movement</div>
                 </div>
                 <div className="flex flex-wrap">
-                <div className="bg-gradient-custom-pink px-4 rounded-md m-1 flex-1 drop-shadow-xl text-white text-sm flex flex-col justify-center items-center">
-                    Since Bed Exit
-                    <p className='text-base font-bold'>{BedExit}</p>
+                    <div className="bg-gradient-custom-pink px-1 rounded-md m-1 flex-1 drop-shadow-xl text-white text-sm flex flex-col justify-center items-center">
+                        Since Bed Exit
+                        <p className='text-base font-bold'>{BedExit}</p>
+                    </div>
+                    <div className="bg-white p-2 rounded-md m-1 flex-1 drop-shadow-xl text-[#4138D0] text-sm flex flex-col justify-center items-center">
+                    Status
+                    <div className="flex justify-around w-full">
+            <div className='text-white text-xs bg-[#FB0000] flex justify-center items-center w-1/2 h-5 rounded-full mr-1'>High HR</div>
+            <div className='text-white text-xs bg-gradient-custom flex justify-center items-center w-1/2 h-5 rounded-full'>VISITOR</div>
+        </div>
                 </div>
-                    <div className="bg-white p-5 rounded-md m-1 flex-1 drop-shadow-xl text-[#4138D0] text-sm">
-                        Status
-                        
-                        </div>
                 </div>
             </div>
         </div>
