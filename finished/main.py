ef9f5adbd8a0649320ad8ed236accf37fec7e93f
Natthakit-Kong <148045102+Natthakit-Kong@users.noreply.github.com>
2023-11-26 12:02:37 +0700

    Update main.py

--- finished/main.py
+++ finished/main.py
@@ -1,1 +1,33 @@
-'https://www.kaggle.com/code/natthakitcheawketwit/arrhythmia-on-ecg-classification-using-cnn'
+# model
+from test import predict as model_predict
+# Web Server
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from typing import List
+import pandas as pd
+
+# create app
+app = FastAPI()
+# alow cross origin all origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# index route
+@app.get('/')
+def index():
+    return {'message': 'This is a sentiment analysis model API.'}
+
+# predict route
+@app.post('/predict')
+def predict():
+    return model_predict()
+
+
+# start server
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
