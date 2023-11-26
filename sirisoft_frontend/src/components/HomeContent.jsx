c90f80f5b263f676d534f7ddbbd71c6b753cb673
Panupong Kaewkhao <tongfah@Panupongs-MacBook-Air.local>
2023-11-26 07:33:46 +0700

    Final demo

--- sirisoft_frontend/src/components/HomeContent.jsx
+++ sirisoft_frontend/src/components/HomeContent.jsx
@@ -5,65 +5,25 @@ import { faHeartPulse, fas } from "@fortawesome/free-solid-svg-icons";
 
 
 function DetailModal({ item, onClose }) {
-    if (!item) return null; // Ensure item is not null
-  
-    return (
-        <div className="bg-[#F6F6F6] rounded-lg shadow-lg p-4 mb-4 absolute flex flex-col w-[1000px] h-[560px]">
-            <div className="bg-gradient-custom text-white py-1 px-4 rounded-t-lg absolute top-0 left-0 right-0">
-                <span>{item.topBarText}</span>
-            </div>
+  if (!item) return null; // Ensure item is not null
 
-            <div className="flex mt-6">
-                <img src={item.imageUrl} alt={item.title} className="w-10 h-10 object-cover mb-2" />
-                <div className="ml-3">
-                    <h2 className="text-md mt-2">{item.title}</h2>
-                </div>
-            </div>
+  return (
+      <div className="bg-[#F6F6F6] rounded-lg shadow-lg p-4 mb-4 absolute flex flex-col w-[1000px] h-[560px]">
+          <div className="bg-gradient-custom text-white py-1 px-4 rounded-t-lg absolute top-0 left-0 right-0">
+              <span>{item.topBarText}</span>
+          </div>
 
-            {/* Status Boxes */}
-            <div className="mt-2 flex-grow">
-                <div className="flex flex-wrap">
-                    <div className="bg-gradient-custom p-4 rounded-md m-1 flex-1 drop-shadow-xl text-white text-sm flex flex-col items-start">
-                        <div className="flex items-center">
-                            <FontAwesomeIcon icon={faHeartPulse} className="text-[#FDD9FF] bg-black p-1.5 rounded-full text-2xl mr-2" />
-                            <span className='mt-[-18px]'>HeartBeat</span>
-                        </div>
-                        <div className="ml-11 mt-[-20px]">
-                            <p style={{ display: 'inline' }} className='font-bold text-2xl'>{item.HeartBeat}</p>
-                            <p style={{ display: 'inline' }}> BPM</p>
-                        </div>
-                    </div>
+          <div className="flex mt-6">
+              <img src="/profile_zoom.png" alt={item.title} className="w-full h-full object-cover" />
+          </div>
 
-                    <div className="bg-gradient-custom p-4 rounded-md m-1 flex-1 drop-shadow-xl text-white text-sm flex flex-col items-start">
-                        <div className="flex items-center">
-                            
-                        <img src="/walk.png" alt="Logo" className="h-12 w-12"/>
-                            <span className='mt-[-48px] ml-2'>Movement</span>
-                        </div>
-                        <div className="ml-14 mt-[-20px]">
-                        <img src="/walk_level.png" alt="Logo" className='mt-[-10px]'/>
-                            <p style={{ display: 'inline' }} className='text-xs '>Last Move: 20m ago</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-wrap">
-                    <div className="bg-gradient-custom-pink px-1 rounded-md m-1 flex-1 drop-shadow-xl text-white text-sm flex flex-col justify-center items-center">
-                        Since Bed Exit
-                        <p className='text-base font-bold'>{item.BedExit}</p>
-                    </div>
-                    <div className="bg-white p-2 rounded-md m-1 flex-1 drop-shadow-xl text-[#4138D0] text-sm flex flex-col justify-center items-center">
-                        Status
-                        <div className="flex justify-around w-full">
-                            <div className='text-white text-xs bg-[#FB0000] flex justify-center items-center w-1/2 h-5 rounded-full mr-1'>High HR</div>
-                            <div className='text-white text-xs bg-gradient-custom flex justify-center items-center w-1/2 h-5 rounded-full'>VISITOR</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <button onClick={onClose}>Back</button>
-        </div>
-    );
-  }
+          {/* Centering the button */}
+          <div className="flex justify-center items-center mt-[-30px]">
+              <button onClick={onClose} className='bg-gradient-custom w-20 h-10 rounded-full text-white'>Back</button>
+          </div>
+      </div>
+  );
+}
 
 
   function HomeContent({ filter }) {
@@ -85,7 +45,8 @@ function DetailModal({ item, onClose }) {
       statusHighHR: true,
       statusVisitor: false,
       statusNeedVisitor: true,
-      statusNormal: false
+      statusNormal: false,
+      RespiratoryRate: "20 BPM"
     },
     {
       title: "Papinwit​  Maneesr",
@@ -97,7 +58,8 @@ function DetailModal({ item, onClose }) {
       statusHighHR: true,
       statusVisitor: true,
       statusNeedVisitor: false,
-      statusNormal: false
+      statusNormal: false,
+      RespiratoryRate: "30 BPM"
     },
     {
       title: "Panupong Kaewkhao",
@@ -109,7 +71,8 @@ function DetailModal({ item, onClose }) {
       statusHighHR: true,
       statusVisitor: true,
       statusNeedVisitor: false,
-      statusNormal: false
+      statusNormal: false,
+      RespiratoryRate: "23 BPM"
     },
     {
       title: "Natthakit Cheawketwit",
@@ -121,7 +84,8 @@ function DetailModal({ item, onClose }) {
       statusHighHR: false,
       statusVisitor: false,
       statusNeedVisitor: true,
-      statusNormal: true
+      statusNormal: true,
+      RespiratoryRate: "29 BPM"
     },
     
 
